8489b94473cae3042dd0d84a9989fcfde62302c5
백호동 <ghehd2311@gmail.com>
2020-08-12 18:29:36 +0900

    First steps with Typescript

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log('test');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = 'ghehd', age = 24, gender = 'male';
+const sayHi = (name, age, gender) => {
+    console.log(`hello ${name}, ${age}, ${gender}`);
+};
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map

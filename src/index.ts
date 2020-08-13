a2813da92af2507246cbe87162db42eb963fac0e
백호동 <ghehd2311@gmail.com>
2020-08-13 16:48:27 +0900

    #1. Typescript에서 class사용하기

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-interface Human {
-  //javascript에서는 작동 안됌(typescript의 코드)
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  //constructor : 생성자 => 클래스가 시작할때마다 호출(클래스로 부터 객체를 만들때마다)
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
+
+const lynn = new Human('Lynn', 22, 'female');
 
 const person = {
   name: 'ghehd',
@@ -11,10 +19,9 @@ const person = {
   gender: 'male',
 };
 
-// const sayHi = (name: string, age: number, gender: string): string => {
 const sayHi = (person: Human): string => {
   return `hello ${person.name}, ${person.age}, ${person.gender}`;
 };
-// console.log(sayHi('ghehd', 444, 'male222'));
-console.log(sayHi(person));
+
+console.log(sayHi(lynn));
 export {};

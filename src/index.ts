ecb9605ef818e2e3402ad0df53cb32870a539e28
백호동 <ghehd2311@gmail.com>
2020-08-13 17:49:18 +0900

    Creating a Block part Two

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
+import * as CryptoJs from 'crypto-js';
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+  // 일반 method : 블록을 생성했을 때만 사용 가능
+  // sayHello() => return 'hello';
+
+  // static method : 클래스가 생성되지 않았어도 호출 할 수 있는 method
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string,
+  ): string => CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
     this.index = index;
     this.hash = hash;
@@ -12,12 +24,22 @@ class Block {
     this.timestamp = timestamp;
   }
 }
+
+// Block.calculateBlockHash() => static method 이므로 Block클래스로부터 바로 사용할 수 있음
 const genesisBlock: Block = new Block(0, '23232323', '', 'Hello', 12345);
 
-// block-chain : 블록의 연걸
-// let blockChain: [Block] ==> typescript에서 블록만 블록체인에 추가하도록 체크함
-// blockChain.push('block') => x
-let blockChain: [Block] = [genesisBlock];
+let blockChain: Block[] = [genesisBlock];
 
-console.log(blockChain);
+//블록체인이 얼마나 긴지 알기 위한 함수
+const getBlockchain = (): Block[] => blockChain; //blockChain를 배열 형태로 리턴
+
+//마지막 블록을 리턴 하는 함수
+const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
+
+//1597308260의 형식으로 숫자 리턴 하는 함수
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+console.log(getBlockchain());
+console.log(getLatestBlock());
+console.log(getNewTimeStamp());
 export {};
